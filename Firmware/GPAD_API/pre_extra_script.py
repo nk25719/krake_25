81d118d8148156eb87c7dad30ba04392b2380de1
nk25719 <naghamkheir25@gmail.com>
2026-05-09 15:54:58 +0300

    Releasing FIrmware V0.57 and LittleFS 0.1.7 .

--- Firmware/GPAD_API/pre_extra_script.py
+++ Firmware/GPAD_API/pre_extra_script.py
@@ -4,7 +4,7 @@ cpp_defines = [
     ("COMPANY_NAME", "PubInv "),   # For the Broker ID for MQTT 
     ("PROG_NAME", "GPAD_API "),    # This program
     ("FIRMWARE_VERSION", "0.57 "), # Refactored so that MQTT is serviced first.
-    ("LittleFS_VERSION", "0.1.6 "), # pr 501
+    ("LittleFS_VERSION", "0.1.7 "), # pr 501
     ("MODEL_NAME", "KRAKE_"), 
     ("LICENSE", "GNU Affero General Public License, version 3 "),
     ("ORIGIN", "US"),
